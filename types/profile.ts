14bd13baf8e1f56e3c73160757d86a41a1f93f0d
unknown <atcoding4@gmail.com>
2026-02-22 11:15:15 +0700

    fix: restore exam 3 and exam 4 fields

--- types/profile.ts
+++ types/profile.ts
@@ -26,6 +26,22 @@ export interface Profile {
   score_reading2: number | null; // int4
   total_score2: number | null; // float4
 
+  structure3?: boolean; // bool
+  listening3?: boolean; // bool
+  reading3?: boolean; // bool
+  score_listening3?: number | null; // int4
+  score_structure3?: number | null; // int4
+  score_reading3?: number | null; // int4
+  total_score3?: number | null; // float4
+
+  structure4?: boolean; // bool
+  listening4?: boolean; // bool
+  reading4?: boolean; // bool
+  score_listening4?: number | null; // int4
+  score_structure4?: number | null; // int4
+  score_reading4?: number | null; // int4
+  total_score4?: number | null; // float4
+
   tanggal_selesai_ujian: string | null; // date
   kategori_soal: number | null; // int2
   tanggal_cetak_sertifikat?: string | null; // date
@@ -57,6 +73,22 @@ export interface CreateProfilePayload {
   score_structure2?: number | null;
   score_reading2?: number | null;
   total_score2?: number | null;
+
+  structure3?: boolean;
+  listening3?: boolean;
+  reading3?: boolean;
+  score_listening3?: number | null;
+  score_structure3?: number | null;
+  score_reading3?: number | null;
+  total_score3?: number | null;
+
+  structure4?: boolean;
+  listening4?: boolean;
+  reading4?: boolean;
+  score_listening4?: number | null;
+  score_structure4?: number | null;
+  score_reading4?: number | null;
+  total_score4?: number | null;
 
   tanggal_selesai_ujian?: string | null;
   kategori_soal?: number | null;
